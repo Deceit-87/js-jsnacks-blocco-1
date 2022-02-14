588ec44b791fd0356dbcd782f1c1c51fbf991667
Deceit-87 <manuelfreund318@gmail.com>
2022-02-14 18:33:59 +0100

    snack9

---
+++ snack9/app.js
@@ -0,0 +1,20 @@
+
+
+
+const NUMERI = [1,2,3,4,5,6,7,8,9,10]
+
+console.log(NUMERI);
+
+
+let somma = 0;
+
+
+
+for (let i = 0; i < NUMERI.length; i++) {
+   somma += NUMERI[i] 
+    
+}
+
+
+let media = somma / NUMERI.length
+console.log(media)
