37828f8aac4609ab8171a996fa24715e9a1aa4ba
Deceit-87 <manuelfreund318@gmail.com>
2022-02-14 18:19:04 +0100

    sna k5 with boolean

--- snack5/app.js
+++ snack5/app.js
@@ -2,9 +2,38 @@
 
 
 
+// const NUMERI = []
+
+
+
+// for (let i = 0; i < 6; i++) {
+//  let numero = parseInt( prompt('inserisci un numero'))
+
+//  console.log(numero)
+    
+//  if (numero % 2 === 0){
+//    console.log('numero pari')
+
+//  }
+//   else{
+
+//     console.log('numero dispari')
+//     NUMERI.push(numero)
+//   }
+
+   
+
+//  }
+
+// console.log(NUMERI);
+
+
+                          // WITH BOOLEAN
+
+
 const NUMERI = []
 
-
+let DISPARI = true
 
 for (let i = 0; i < 6; i++) {
  let numero = parseInt( prompt('inserisci un numero'))
@@ -14,10 +43,14 @@ for (let i = 0; i < 6; i++) {
  if (numero % 2 === 0){
    console.log('numero pari')
 
- }
-  else{
+   DISPARI = false
 
+ }
+ else{
+    
+   
     console.log('numero dispari')
+    
     NUMERI.push(numero)
   }
 
