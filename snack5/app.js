215d92775d9a09d5631912495645dbb761457005
Deceit-87 <manuelfreund318@gmail.com>
2022-02-14 17:45:32 +0100

    snack 5

---
+++ snack5/app.js
@@ -0,0 +1,17 @@
+
+
+
+
+const numeri = []
+
+
+
+for (let i = 0; i < 6; i++) {
+ let numero = parseFloat( prompt('inserisci un numero'))
+
+ console.log(numero)
+    
+ if (numero % 3){
+   
+ }
+}
